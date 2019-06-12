36ad71c3235f322a153f9bd4bb6107369cc85d75
Andrew Aluko <aluko17@icloud.com>
2019-06-12 12:02:05 -0500

    youtube component

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,23 +1,32 @@
 import React, { Component } from 'react';
+import YouTube from 'react-youtube';
 
 
 
 export default class Contact extends Component {
-	render(){
-		return(
-			<div>
+    render() {
+        const opts = {
+            height: '390',
+            width: '640',
+            playerVars: { // https://developers.google.com/youtube/player_parameters
+                autoplay: 1
+            }
+        };
+        return (
+            <div>
 				
 				<div className="container">	
 					<h2>Social</h2>
-					<p>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-						</p>
-						<p>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ac odio tempor orci dapibus ultrices in iaculis. Sed viverra ipsum nunc aliquet bibendum enim facilisis gravida neque. Nulla facilisi morbi tempus iaculis. Scelerisque eu ultrices vitae auctor eu augue ut lectus. Velit euismod in pellentesque massa placerat duis ultricies lacus sed. Nec sagittis aliquam malesuada bibendum arcu vitae elementum. Leo urna molestie at elementum eu facilisis sed odio. Sed viverra tellus in hac habitasse platea dictumst vestibulum rhoncus. Nunc lobortis mattis aliquam faucibus purus in. Orci nulla pellentesque dignissim enim sit amet venenatis. Id ornare arcu odio ut. Venenatis lectus magna fringilla urna porttitor rhoncus dolor purus. Vitae auctor eu augue ut lectus arcu bibendum at varius. Duis at tellus at urna. Purus sit amet luctus venenatis lectus.
-						</p>
+									<YouTube
+				        videoId="2g811Eo7K8U"
+				        opts={opts}
+				        onReady={this._onReady}
+						/>
+					 
+			  
 					</div>
 						
 			</div>
-			);
-	}
+        );
+    }
 }
