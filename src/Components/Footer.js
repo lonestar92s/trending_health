f0e637b1b9d4a4fd60563dbda827259c435f2162
Andrew Aluko <aluko17@icloud.com>
2019-06-26 13:22:42 -0500

    nav bar items

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -6,13 +6,15 @@ export default class Footer extends Component {
         return (
         <footer className="footer">
 		      <div>
-		        <nav className="items">
+		        <nav className="Items">
 		        <ul>
 		        <li><span>Trending Health &#169; {new Date().getFullYear()}</span></li>
 		        </ul>
-		        <span><a href ="#" className="social"><i className="instagram icon"></i></a></span>
-		        <span><a href ="#" className="social"><i className="twitter icon"></i></a></span>
-		        <span><a href ="#" className="social"><i className="linkedin icon"></i></a></span>
+		        <ul className="Links">
+		        <li className="footer-item"><span><a href ="#" className="social"><i className="instagram icon"></i></a></span></li>
+		        <li className="footer-item"><span><a href ="#" className="social"><i className="twitter icon"></i></a></span></li>
+		        <li className="footer-item"><span><a href ="#" className="social"><i className="linkedin icon"></i></a></span></li>
+		        </ul>
 		        </nav> 
 
 		        	
