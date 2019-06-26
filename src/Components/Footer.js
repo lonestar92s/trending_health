34b9f7984b7b398e84be6608c63800ac2f2c11eb
Andrew Aluko <aluko17@icloud.com>
2019-06-26 13:11:44 -0500

    footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,11 +5,15 @@ export default class Footer extends Component {
     render() {
         return (
         <footer className="footer">
-		      <div className="container">
-		        <span>Trending Health &#169; {new Date().getFullYear()}</span>
+		      <div>
+		        <nav className="items">
+		        <ul>
+		        <li><span>Trending Health &#169; {new Date().getFullYear()}</span></li>
+		        </ul>
 		        <span><a href ="#" className="social"><i className="instagram icon"></i></a></span>
 		        <span><a href ="#" className="social"><i className="twitter icon"></i></a></span>
-		        <span><a href ="#" className="social"><i className="linkedin icon"></i></a></span> 
+		        <span><a href ="#" className="social"><i className="linkedin icon"></i></a></span>
+		        </nav> 
 
 		        	
 		       
