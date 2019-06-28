5fc348d9c9c9c1b74570607ad430d4da3fb1cbf2
Andrew Aluko <aluko17@icloud.com>
2019-06-28 11:53:52 -0500

    email page

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -27,6 +27,9 @@ export default class Navbar extends Component {
 			          <li className="nav-item">
 			            <Link className="nav-link" to="/social">Social</Link>
 			          </li>
+			           <li className="nav-item">
+			            <Link className="nav-link" to="/resources">Resources</Link>
+			          </li>
 			        </ul>
 		       </div>
 		    </div>
---
+++ src/Pages/Email.js
@@ -0,0 +1,5 @@
+import React, { Component } from 'react';
+
+export default class Email extends Component{
+	
+}
